e2ac1e06edf0b6431d7cd77c25ca203513cac4d7
yanorei32 <yanorei@hotmail.co.jp>
2026-08-24 20:43:23 +0900

    Bump up serde-xml-rs to 0.8.2

--- web-ir-remote/src/config.rs
+++ web-ir-remote/src/config.rs
@@ -4,36 +4,35 @@ use serde::Deserialize;
 
 #[derive(Debug, Clone, Deserialize)]
 pub struct Controllers {
-    #[serde(rename = "$value")]
+    #[serde(rename = "Controller", default)]
     pub controllers: Vec<Controller>,
 }
 
 #[derive(Debug, Clone, Deserialize)]
 pub struct Controller {
-    #[serde(rename = "$value")]
+    #[serde(rename = "#content")]
     pub root_node: Node,
-
-    #[allow(dead_code)]
-    pub id: String,
 }
 
 #[derive(Debug, Clone, Deserialize)]
 pub struct Button {
+    #[serde(rename = "@label")]
     #[allow(dead_code)]
     pub label: String,
 
+    #[serde(rename = "@action")]
     pub action: PathBuf,
 }
 
 #[derive(Debug, Clone, Deserialize)]
 pub struct VBox {
-    #[serde(rename = "$value")]
+    #[serde(rename = "#content")]
     pub children: Vec<Node>,
 }
 
 #[derive(Debug, Clone, Deserialize)]
 pub struct HBox {
-    #[serde(rename = "$value")]
+    #[serde(rename = "#content")]
     pub children: Vec<Node>,
 }
 
